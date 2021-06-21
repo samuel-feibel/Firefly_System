9073860282148924a64a71f948d8c5524f13b010
samuel-feibel <sof23@cornell.edu>
2021-06-20 23:30:53 -0400

    added rest of measurement vector generation. Untested.

--- Microcontroller_Code/autoPilot/include/estimateState.h
+++ Microcontroller_Code/autoPilot/include/estimateState.h
@@ -3,6 +3,10 @@
 
 void LatLonAlt2NED_Fcn(double Lat, double Lon, double Alt, double r0_ECEF, double NED_C_ECEF, double *r_NED);
 
+void setAngle2Range(double *angle);
+
+void setupP0ECEF();
+
 void getMeasurementVector(double *Z, double *Z_input);
 
 void LatLonAlt2ECEF_Fcn(double Lat, double Lon, double Alt, double *r0_ECEF);
